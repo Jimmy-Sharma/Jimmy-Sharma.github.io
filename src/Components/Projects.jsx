da21f2c2f3c969776ac7cdaecef42e1d60832a17
Prince Sharma <prince786no.1@gmail.com>
2023-03-13 22:01:09 +0530

    Update

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -26,7 +26,7 @@ const Projects = () => {
                     <a
                       className="project-deployed-link"
                       style={{ textDecoration: "none" }}
-                      href="https://Jimmy-Sharma.github.io/beneficial-jelly-4258/"
+                      href="https://jimmy-sharma.github.io/beneficial-jelly-4258/"
                     >
                       Check it Out!
                     </a>
