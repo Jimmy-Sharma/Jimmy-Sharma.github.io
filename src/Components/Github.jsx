2e6f2e3df227ca7007d0e52cae40416c69df345d
Prince Sharma <prince786no.1@gmail.com>
2023-03-13 21:05:19 +0530

    Update

--- src/Components/Github.jsx
+++ src/Components/Github.jsx
@@ -5,18 +5,18 @@ import '../Styling/Github.css'
 const Github = () => {
   return (
     <div className='githubStat' style={{ display: "grid", alignItems: "center", justifyContent: "center" }}>
-      <GithubCalender  color={"orange"} username='i-m-prince'>
+      <GithubCalender  color={"orange"} username='Jimmy-Sharma'>
 
       </GithubCalender>
       <div style={{ display: "grid", alignItems: "center", justifyContent: "center" ,gap:'20px', marginTop:"40px"}}>
         <div>
-          <img id="github-streak-stats" src="https://github-readme-streak-stats.herokuapp.com/?user=i-m-prince&theme=dark&hide_border=false" alt="" />
+          <img id="github-streak-stats" src="https://github-readme-streak-stats.herokuapp.com/?username=Jimmy-Sharma&theme=dark&hide_border=false" alt="" />
         </div>
         <div>
-          <img id='github-top-langs' src="https://github-readme-stats.vercel.app/api/top-langs/?username=i-m-prince&theme=dark&hide_border=false&include_all_commits=true&count_private=true&layout=compact" alt="" />
+          <img id='github-top-langs' src="https://github-readme-stats.vercel.app/api/top-langs/?username=Jimmy-Sharma&theme=dark&hide_border=false&include_all_commits=true&count_private=true&layout=compact" alt="" />
         </div>
         <div>
-          <img id="github-stats-card" src="https://github-readme-stats.vercel.app/api?username=i-m-prince&theme=dark&hide_border=false&include_all_commits=true&count_private=true" alt="" />
+          <img id="github-stats-card" src="https://github-readme-stats.vercel.app/api?username=Jimmy-Sharma&theme=dark&hide_border=false&include_all_commits=true&count_private=true" alt="" />
         </div>
       </div>
     </div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
           </div>
           <HomeText />
           <div className="contact" data-aos="fade-up">
-            <a id="contact-github" href="https://github.com/i-m-prince" target="blank"><FaGithub className="icon" /></a>
+            <a id="contact-github" href="https://github.com/Jimmy-Sharma" target="blank"><FaGithub className="icon" /></a>
             <a id="contact-linkedin" href="https://www.linkedin.com/in/princesharma1607/" target="blank"><GrLinkedin className="icon" /></a>
             {/* <button id="resume-button-2" onClick={handleClickHome}>
               <a id="resume-link-2"
